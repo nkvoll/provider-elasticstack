55aadb42a5d7d3c34e99a5d372ed9bae8537ee54
Njal Karevoll <njal@karevoll.no>
2023-09-22 13:09:24 +0200

    make short group names constants

--- config/provider.go
+++ config/provider.go
@@ -14,6 +14,10 @@ import (
 const (
 	resourcePrefix = "elasticstack"
 	modulePath     = "github.com/elastic/provider-elasticstack"
+
+	elasticsearchShortGroup = "elasticsearch"
+	fleetShortGroup         = "fleet"
+	kibanaShortGroup        = "kibana"
 )
 
 //go:embed schema.json
@@ -36,7 +40,7 @@ func GetProvider() *ujconfig.Provider {
 		// add custom config functions
 		func(p *ujconfig.Provider) {
 			p.AddResourceConfigurator("elasticstack_kibana_action_connector", func(r *ujconfig.Resource) {
-				r.ShortGroup = "kibana"
+				r.ShortGroup = kibanaShortGroup
 
 				// Mark secrets as sensitive -- this should probably be changed in the elasticstack provider?
 				r.TerraformResource.Schema["secrets"].Sensitive = true
@@ -47,7 +51,7 @@ func GetProvider() *ujconfig.Provider {
 				}
 			})
 			p.AddResourceConfigurator("elasticstack_kibana_alerting_rule", func(r *ujconfig.Resource) {
-				r.ShortGroup = "kibana"
+				r.ShortGroup = kibanaShortGroup
 
 				r.References["actions.id"] = ujconfig.Reference{
 					Type:      "github.com/elastic/provider-elasticstack/apis/kibana/v1alpha1.ActionConnector",
@@ -59,7 +63,7 @@ func GetProvider() *ujconfig.Provider {
 				}
 			})
 			p.AddResourceConfigurator("elasticstack_kibana_slo", func(r *ujconfig.Resource) {
-				r.ShortGroup = "kibana"
+				r.ShortGroup = kibanaShortGroup
 
 				r.References["space_id"] = ujconfig.Reference{
 					Type:      "github.com/elastic/provider-elasticstack/apis/kibana/v1alpha1.Space",
@@ -67,7 +71,7 @@ func GetProvider() *ujconfig.Provider {
 				}
 			})
 			p.AddResourceConfigurator("elasticstack_kibana_space", func(r *ujconfig.Resource) {
-				r.ShortGroup = "kibana"
+				r.ShortGroup = kibanaShortGroup
 
 				r.References["space_id"] = ujconfig.Reference{
 					Type:      "github.com/elastic/provider-elasticstack/apis/kibana/v1alpha1.Space",
@@ -77,73 +81,73 @@ func GetProvider() *ujconfig.Provider {
 		},
 		func(p *ujconfig.Provider) {
 			p.AddResourceConfigurator("elasticstack_elasticsearch_cluster_settings", func(r *ujconfig.Resource) {
-				r.ShortGroup = "elasticsearch"
+				r.ShortGroup = elasticsearchShortGroup
 			})
 			p.AddResourceConfigurator("elasticstack_elasticsearch_component_template", func(r *ujconfig.Resource) {
-				r.ShortGroup = "elasticsearch"
+				r.ShortGroup = elasticsearchShortGroup
 			})
 			p.AddResourceConfigurator("elasticstack_elasticsearch_data_stream", func(r *ujconfig.Resource) {
-				r.ShortGroup = "elasticsearch"
+				r.ShortGroup = elasticsearchShortGroup
 			})
 			p.AddResourceConfigurator("elasticstack_elasticsearch_enrich_policy", func(r *ujconfig.Resource) {
-				r.ShortGroup = "elasticsearch"
+				r.ShortGroup = elasticsearchShortGroup
 			})
 			p.AddResourceConfigurator("elasticstack_elasticsearch_index", func(r *ujconfig.Resource) {
-				r.ShortGroup = "elasticsearch"
+				r.ShortGroup = elasticsearchShortGroup
 			})
 			p.AddResourceConfigurator("elasticstack_elasticsearch_index_lifecycle", func(r *ujconfig.Resource) {
-				r.ShortGroup = "elasticsearch"
+				r.ShortGroup = elasticsearchShortGroup
 			})
 			p.AddResourceConfigurator("elasticstack_elasticsearch_index_template", func(r *ujconfig.Resource) {
-				r.ShortGroup = "elasticsearch"
+				r.ShortGroup = elasticsearchShortGroup
 			})
 			p.AddResourceConfigurator("elasticstack_elasticsearch_ingest_pipeline", func(r *ujconfig.Resource) {
-				r.ShortGroup = "elasticsearch"
+				r.ShortGroup = elasticsearchShortGroup
 			})
 			p.AddResourceConfigurator("elasticstack_elasticsearch_logstash_pipeline", func(r *ujconfig.Resource) {
-				r.ShortGroup = "elasticsearch"
+				r.ShortGroup = elasticsearchShortGroup
 			})
 			p.AddResourceConfigurator("elasticstack_elasticsearch_script", func(r *ujconfig.Resource) {
-				r.ShortGroup = "elasticsearch"
+				r.ShortGroup = elasticsearchShortGroup
 			})
 			p.AddResourceConfigurator("elasticstack_elasticsearch_security_api_key", func(r *ujconfig.Resource) {
-				r.ShortGroup = "elasticsearch"
+				r.ShortGroup = elasticsearchShortGroup
 			})
 			p.AddResourceConfigurator("elasticstack_elasticsearch_security_role", func(r *ujconfig.Resource) {
-				r.ShortGroup = "elasticsearch"
+				r.ShortGroup = elasticsearchShortGroup
 			})
 			p.AddResourceConfigurator("elasticstack_elasticsearch_security_role_mapping", func(r *ujconfig.Resource) {
-				r.ShortGroup = "elasticsearch"
+				r.ShortGroup = elasticsearchShortGroup
 			})
 			p.AddResourceConfigurator("elasticstack_elasticsearch_security_system_user", func(r *ujconfig.Resource) {
-				r.ShortGroup = "elasticsearch"
+				r.ShortGroup = elasticsearchShortGroup
 			})
 			p.AddResourceConfigurator("elasticstack_elasticsearch_security_user", func(r *ujconfig.Resource) {
-				r.ShortGroup = "elasticsearch"
+				r.ShortGroup = elasticsearchShortGroup
 			})
 			p.AddResourceConfigurator("elasticstack_elasticsearch_snapshot_lifecycle", func(r *ujconfig.Resource) {
-				r.ShortGroup = "elasticsearch"
+				r.ShortGroup = elasticsearchShortGroup
 			})
 			p.AddResourceConfigurator("elasticstack_elasticsearch_snapshot_repository", func(r *ujconfig.Resource) {
-				r.ShortGroup = "elasticsearch"
+				r.ShortGroup = elasticsearchShortGroup
 			})
 			p.AddResourceConfigurator("elasticstack_elasticsearch_transform", func(r *ujconfig.Resource) {
-				r.ShortGroup = "elasticsearch"
+				r.ShortGroup = elasticsearchShortGroup
 			})
 			p.AddResourceConfigurator("elasticstack_elasticsearch_watch", func(r *ujconfig.Resource) {
-				r.ShortGroup = "elasticsearch"
+				r.ShortGroup = elasticsearchShortGroup
 			})
 
 		},
 		func(p *ujconfig.Provider) {
 			p.AddResourceConfigurator("elasticstack_fleet_agent_policy", func(r *ujconfig.Resource) {
-				r.ShortGroup = "fleet"
+				r.ShortGroup = fleetShortGroup
 			})
 			p.AddResourceConfigurator("elasticstack_fleet_output", func(r *ujconfig.Resource) {
-				r.ShortGroup = "fleet"
+				r.ShortGroup = fleetShortGroup
 			})
 			p.AddResourceConfigurator("elasticstack_fleet_server_host", func(r *ujconfig.Resource) {
-				r.ShortGroup = "fleet"
+				r.ShortGroup = fleetShortGroup
 			})
 		},
 	} {
